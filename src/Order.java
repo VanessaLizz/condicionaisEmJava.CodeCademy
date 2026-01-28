2a27f04579c836f6688725a50e9cbf497a7405d9
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-01-28 09:18:17 -0300

    início de condicionais em java

---
+++ src/Order.java
@@ -0,0 +1,22 @@
+public class Order {
+
+    public static void main(String[] args) {
+
+        double itemCost = 30.99;
+
+        // Write an if-then statement:
+        if (itemCost > 24) {
+            System.out.println("High value item!");
+        }
+
+        boolean isFilled = false;
+        // Write an if-then-else statement:
+        if (isFilled) {
+            System.out.println("Shipping");
+        } else {
+            System.out.println("Order not ready");
+        }
+
+    }
+
+}
