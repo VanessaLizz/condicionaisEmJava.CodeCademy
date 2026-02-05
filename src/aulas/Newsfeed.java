a3c17d25affcbf115736ec409ab3878102973c2f
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-02-05 08:50:02 -0300

    import e Arrays

---
+++ src/aulas/Newsfeed.java
@@ -0,0 +1,13 @@
+package aulas;
+
+public class Newsfeed {
+    public Newsfeed(){
+
+    }
+
+    // Create getTopics() below:
+    public String[] getTopics(){
+        String[] topics = {"Opinion", "Tech", "Science", "Health"};
+        return topics;
+    }
+}
