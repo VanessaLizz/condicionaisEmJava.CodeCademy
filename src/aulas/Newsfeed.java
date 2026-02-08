11e996ca0fd871a47ddf290a8bca89c4a4da176e
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-02-08 20:19:20 -0300

    criação de array vazio

--- src/aulas/Newsfeed.java
+++ src/aulas/Newsfeed.java
@@ -3,8 +3,14 @@ package aulas;
 public class Newsfeed {
     String[] topics = {"Opinion", "Tech", "Science", "Health"};
     public int[] views = {0, 0, 0, 0};
+    String[] favoriteArticles;
 
     public Newsfeed(){
+        String favoriteArticles = new String[10];
+    }
+
+    public void setFavoriteArticle(int favoriteIndex, String newArticle){
+        favoriteArticles[favoriteIndex] = newArticle;
     }
 
     public String[] getTopics(){
@@ -18,4 +24,5 @@ public class Newsfeed {
     public void viewTopic(int topicIndex){
         views[topicIndex]++;
     }
+
 }
