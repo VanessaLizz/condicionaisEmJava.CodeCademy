5a6189f908c25e324a7760cd2523f422f4a88b0c
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-02-16 16:37:24 -0300

    adicionando itens no arrayList

--- src/aulas/ToDos.java
+++ src/aulas/ToDos.java
@@ -3,8 +3,20 @@ import java.util.ArrayList;
 
 
 class ToDos {
+
     public static void main(String[] args) {
+
         ArrayList<String> toDoList = new ArrayList<String>();
+        String toDo1 = "Water plants";
+        String toDo2 = "Water animals";
+        String toDo3 = "Water insects";
+
+        toDoList.add(toDo1);
+        toDoList.add(toDo2);
+        toDoList.add(toDo3);
+
+        System.out.println(toDoList);
+
 
     }
 
