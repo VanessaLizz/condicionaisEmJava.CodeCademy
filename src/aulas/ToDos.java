c815b3a0252d15227ac6e4fd49df8b4fc5b71a28
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-02-18 16:08:13 -0300

    tamanho da arrayList

--- src/aulas/ToDos.java
+++ src/aulas/ToDos.java
@@ -14,10 +14,31 @@ class ToDos {
         toDoList.add(toDo1);
         toDoList.add(toDo2);
         toDoList.add(toDo3);
+        toDoList.add(toDo4);
 
         System.out.println(toDoList);
 
+        ArrayList<String> sherlocksToDos = new ArrayList<String>();
 
+        sherlocksToDos.add("visit the crime scene");
+        sherlocksToDos.add("play violin");
+        sherlocksToDos.add("interview suspects");
+        sherlocksToDos.add("solve the case");
+        sherlocksToDos.add("apprehend the criminal");
+
+        ArrayList<String> poirotsToDos = new ArrayList<String>();
+
+        poirotsToDos.add("visit the crime scene");
+        poirotsToDos.add("interview suspects");
+        poirotsToDos.add("let the little grey cells do their work");
+        poirotsToDos.add("trim mustache");
+        poirotsToDos.add("call all suspects together");
+        poirotsToDos.add("reveal the truth of the crime");
+
+        System.out.println(sherlocksToDos.size());
+        System.out.println(poirotsToDos.size());
+
+        // Print the name of the detective with the larger to-do list:
+        System.out.println("Poirot");
     }
-
 }
