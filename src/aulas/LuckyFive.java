35d5e33992aeda599be4e71df3345a8026f68ca1
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-02-23 19:57:30 -0300

    loops

---
+++ src/aulas/LuckyFive.java
@@ -0,0 +1,19 @@
+package aulas;
+import java.util.Random;
+
+class LuckyFive {
+
+    public static void main(String[] args) {
+
+        // Creating a random number generator
+        Random randomGenerator = new Random();
+
+        // Generate a number between 1 and 6
+        int dieRoll = randomGenerator.nextInt(6) + 1;
+
+        while (dieRoll != 5) {
+            System.out.println(dieRoll);
+            dieRoll = randomGenerator.nextInt(6) + 1;
+        }
+    }
+}
