e9ee03a35014a23e171b624ebd59876ab1d86704
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-02-24 17:29:57 -0300

    while

---
+++ src/aulas/Coffee.java
@@ -0,0 +1,14 @@
+package aulas;
+
+public class Coffee {
+    public static void main(String[] args) {
+
+        int cupsOfCoffee = 1;
+
+        while (cupsOfCoffee <= 100){
+            cupsOfCoffee++;
+            System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+        }
+
+    }
+}
