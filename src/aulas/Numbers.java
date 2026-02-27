b44dda6e2979cd218effca292c6a6c5b2c6fe07b
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-02-27 17:29:15 -0300

    loops

---
+++ src/aulas/Numbers.java
@@ -0,0 +1,14 @@
+package aulas;
+
+class Numbers {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i % 5 != 0){
+                continue;
+            }
+            System.out.print(i);
+
+
+        }
+    }
+}
