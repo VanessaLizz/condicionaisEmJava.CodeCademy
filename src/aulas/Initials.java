06abe690e664c70f33f81c691a9605b9603b6feb
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-03-04 20:33:41 -0300

    04/03/26

---
+++ src/aulas/Initials.java
@@ -0,0 +1,16 @@
+package aulas;
+
+public class Initials {
+
+    public static void main(String[] args) {
+
+        // Add a first name and a last name:
+        String firstName = "Vanessa";
+        String lastName = "Sousa";
+
+        // What are the initials?
+        System.out.println(firstName.charAt(0));
+        System.out.println(lastName.charAt(0));
+    }
+
+}
---
+++ src/aulas/MorseCode.java
@@ -0,0 +1,13 @@
+package aulas;
+
+public class MorseCode {
+
+    public static void main(String[] args) {
+
+        String code = ".... .. / .. .----. -- / ... --- -. -. -.-- / .- -. -.. / .. / .-.. .. -.- . / - --- / . .- - / .-. .- -- . -.";
+
+        // Write the code:
+        System.out.println(code.indexOf(". .- -"));
+    }
+
+}
