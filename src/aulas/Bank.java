18031878a41f338416b184eb0de2bb97e57e02f9
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-03-16 19:18:30 -0300

    início de encapsulamento

---
+++ src/aulas/Bank.java
@@ -0,0 +1,11 @@
+package aulas;
+
+public class Bank{
+    public static void main(String[] args){
+        CheckingAccount accountOne = new CheckingAccount("Zeus", 100);
+        CheckingAccount accountTwo = new CheckingAccount("Hades", 200);
+        CheckingAccount myAccount = new CheckingAccount("Mike", 300);
+
+        myAccount.printBalance();
+    }
+}
