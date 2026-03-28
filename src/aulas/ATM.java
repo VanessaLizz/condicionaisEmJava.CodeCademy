f563ceb5231bf90f101f20ca43c0a18dca9363bf
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-03-28 19:23:28 -0300

    modificação de variaveis estaticas

--- src/aulas/ATM.java
+++ src/aulas/ATM.java
@@ -1,8 +1,7 @@
 package aulas;
 
 public class ATM{
-
-    // Step 2: Create your static variables here
+    // Static variables
     public static int totalMoney = 0;
     public static int numATMs = 0;
 
@@ -11,25 +10,30 @@ public class ATM{
 
     public ATM(int inputMoney){
         this.money = inputMoney;
+
+        // Steps 1 and 2: Edit numATMs and total money here
+        ATM.numATMs += 1;
+        ATM.totalMoney = inputMoney + totalMoney;
     }
 
     public void withdrawMoney(int amountToWithdraw){
         if(amountToWithdraw <= this.money){
             this.money -= amountToWithdraw;
+            // Step 3: Edit totalMoney here
+            totalMoney -= amountToWithdraw;
         }
     }
 
     public static void main(String[] args){
-        // Step 1: Create your two ATMs here
+
+        System.out.println("Total number of ATMs: " + ATM.numATMs);
         ATM firstATM = new ATM(1000);
         ATM secondATM = new ATM(500);
-        System.out.println(firstATM);
-        System.out.println(secondATM);
+        System.out.println("Total number of ATMs: " + ATM.numATMs);
 
-        // Step 3: Print your static variable in three different ways here
-        System.out.println(ATM.totalMoney);
-        System.out.println(firstATM.totalMoney);
-        System.out.println(secondATM.totalMoney);
+        System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney);
+        firstATM.withdrawMoney(500);
+        secondATM.withdrawMoney(200);
+        System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney);
     }
-
 }
