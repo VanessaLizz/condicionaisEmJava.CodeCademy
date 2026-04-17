4e5f3157573fcd6c1f5d8b505a1695a58ef3dcec
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-04-17 09:32:51 -0300

    correção de bugs com erro de execução

--- src/aulas/Debug.java
+++ src/aulas/Debug.java
@@ -1,10 +1,15 @@
 package aulas;
 
 public class Debug {
-    public static void main(String[] args) {
+    int width = 20;
+    int length = 40;
+    int ratio = length / width;
+
+    public void main(String[] args) {
         System.out.println("       1");
         System.out.println("     2 3");
         System.out.println("   4 5 6");
         System.out.println("7 8 9 10");
+        System.out.println("20 / 40 (Width / Length) = " + ratio);
     }
 }
