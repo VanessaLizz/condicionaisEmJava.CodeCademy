7067d890b60cf029042642dfb9034e24ca369515
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-04-17 09:54:30 -0300

    verificação de exceções

--- src/aulas/Debug.java
+++ src/aulas/Debug.java
@@ -11,5 +11,9 @@ public class Debug {
         System.out.println("   4 5 6");
         System.out.println("7 8 9 10");
         System.out.println("20 / 40 (Width / Length) = " + ratio);
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        int lastNumber = numbers[4];
+        System.out.println("The value of the last element is: " + lastNumber);
     }
 }
