f0e9449e790e20dd435e9e1d7f182cf61c243b80
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-04-17 10:12:24 -0300

    tratamento de exceções

--- src/aulas/Debug.java
+++ src/aulas/Debug.java
@@ -15,5 +15,11 @@ public class Debug {
         int[] numbers = {1, 2, 3, 4, 5};
         int lastNumber = numbers[4];
         System.out.println("The value of the last element is: " + lastNumber);
+
+        try {
+            int ratio = length / width;
+        } catch (ArithmeticException e) {
+            System.err.println("ArithmeticException: " + e.getMessage());
+        }
     }
 }
