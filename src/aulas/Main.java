f0675e3390613f021196e453f5f7dab379dd979c
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-04-20 08:27:51 -0300

    declaração de MB de char

--- src/aulas/Main.java
+++ src/aulas/Main.java
@@ -49,5 +49,8 @@ public class Main{
         // Declare a 2d array of float values called floatTwoD
         float[][] floatTwoD;
         floatTwoD = new float[4][10];
+
+        char[][] ticTacToe = {{'X', 'O', 'O'}, {'O', 'X', ' '}, {'X', ' ', 'X'}};
+        ticTacToe = new char[][] {{'O', 'X', 'X'}, {'X', 'O', ' '}, {'O', ' ', 'O'}};
     }
 }
