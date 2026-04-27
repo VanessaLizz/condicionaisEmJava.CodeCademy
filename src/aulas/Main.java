3183c1d356f6137e559185c2961c84cab810e91f
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-04-27 09:47:39 -0300

    soma de for interno e externo

--- src/aulas/Main.java
+++ src/aulas/Main.java
@@ -88,7 +88,7 @@ public class Main{
         }
 
         //Given the provided 2d array
-        int[][] intMatrix = {
+        int[][] intMatrix2 = {
                 { 4,  6,  8, 10, 12, 14, 16},
                 {18, 20, 22, 24, 26, 28, 30},
                 {32, 34, 36, 38, 40, 42, 44},
@@ -96,14 +96,16 @@ public class Main{
                 {60, 62, 64, 66, 68, 70, 79}
         };
 
-        int rows = intMatrix.length;
-        int columns = intMatrix[0].length;
+        int rows = intMatrix2.length;
+        int columns = intMatrix2[0].length;
+
         int sum = 0;
-        for(int i=0; i<-1; i++) {
-            for(int j = 0; j < -1; j++) {
+        for(int i=0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
                 // Add a line to calculate sum of all elements
+                sum+=intMatrix2[i][j];
             }
-        }
         System.out.println(sum);
+        }
     }
 }
