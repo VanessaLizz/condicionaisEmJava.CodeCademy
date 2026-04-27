8c5dafe27ed06b4ce21f60317db8816494a7d5e2
Vanessa Liz <vanessalizsousa1996@gmail.com>
2026-04-27 09:42:29 -0300

    criação de variaveis para row and columns

--- src/aulas/Main.java
+++ src/aulas/Main.java
@@ -86,5 +86,24 @@ public class Main{
             }
             System.out.println();
         }
+
+        //Given the provided 2d array
+        int[][] intMatrix = {
+                { 4,  6,  8, 10, 12, 14, 16},
+                {18, 20, 22, 24, 26, 28, 30},
+                {32, 34, 36, 38, 40, 42, 44},
+                {46, 48, 50, 52, 54, 56, 58},
+                {60, 62, 64, 66, 68, 70, 79}
+        };
+
+        int rows = intMatrix.length;
+        int columns = intMatrix[0].length;
+        int sum = 0;
+        for(int i=0; i<-1; i++) {
+            for(int j = 0; j < -1; j++) {
+                // Add a line to calculate sum of all elements
+            }
+        }
+        System.out.println(sum);
     }
 }
